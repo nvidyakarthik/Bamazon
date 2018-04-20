166ee9c6a8c20e6e726f362967075beb75bcca86
nvidya.karthik <nvidya.karthik@gmail.com>
2018-04-20 12:36:07 -0700

    conditions added for negative,0 quanitity and wrong record number

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,7 +15,7 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     listProducts();
-    
+
 
 
 
@@ -34,73 +34,91 @@ function listProducts() {
             tableFormat.addRow(res[index].item_id, res[index].product_name, res[index].price);
         console.log(tableFormat.toString());
         userInput();
-        
+
     });
 
 
 
 }
-function userInput(){
+function userInput() {
     inquirer.prompt([
 
         {
-          type: "input",
-          name: "itemId",
-          message: "Please Enter the Item Id."
+            type: "input",
+            name: "itemId",
+            message: "Please Enter the Item Id."
         },
         {
             type: "input",
             name: "quantity",
             message: "Please Enter the Number of Units you would like to purchase."
         }
-    ]).then(function(response) {
+    ]).then(function (response) {
         console.log(response.itemId);
-        checkQuantity(response.itemId,response.quantity);
-        
+        checkQuantity(response.itemId, Number(response.quantity));
+        connection.end();
 
-    
+
+
+
     });
 
 }
-function checkQuantity(itemId,quantityReq){
+function checkQuantity(itemId, quantityReq) {
+
+    connection.query("SELECT stock_quantity,price FROM products WHERE item_id=?", [itemId], function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        /* console.log(res);
+        console.log(err); */
+        if (res.length != 0) {
+            if (quantityReq <= res[0].stock_quantity && quantityReq > 0) {
+                var remainingQuanity = res[0].stock_quantity - quantityReq;
+                updateQuanity(itemId, remainingQuanity);
+                console.log("Your Total cost is $" + res[0].price * quantityReq);
+            }
+            else if (quantityReq < 0) {
+                console.log("Please enter valid quantity.");
+            }
+            else if (quantityReq == 0) {
+                console.log("You entered " + quantityReq + " Please enter the correct quantity.");
+
+            }
+            else {
+                console.log("Insufficient quantity!");
+            }
+
+        }
+        else {
+            console.log("Please enter valid Item Id number");
+        }
+
+
+    });
     
-        connection.query("SELECT stock_quantity,price FROM products WHERE item_id=?",[itemId], function(err, res) {
-          if (err) throw err;
-          // Log all results of the SELECT statement
-          console.log(res);
-          if(quantityReq<=res[0].stock_quantity){
-             var remainingQuanity= res[0].stock_quantity-quantityReq;
-             updateQuanity(itemId,remainingQuanity);
-              console.log("Your Total cost is $"+res[0].price*quantityReq);
-          }
-          else{
-              console.log("Insufficient quantity!");
-          }
-           
-              
-        });
+
+
 
 }
 
-function updateQuanity(itemId,quantity){
-    
+function updateQuanity(itemId, quantity) {
+
     var query = connection.query(
-      "UPDATE products SET ? WHERE ?",
-      [
-        {
-          stock_quantity: quantity
-        },
-        {
-          item_id: itemId
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: quantity
+            },
+            {
+                item_id: itemId
+            }
+        ],
+        function (err, res) {
+            console.log(err);
+            console.log(res.affectedRows + " products updated!\n");
+           
         }
-      ],
-      function(err, res) {
-        console.log(err);
-        console.log(res.affectedRows + " products updated!\n");
-        connection.end();
-        
-      }
     );
-  
-    
+
+
 }
