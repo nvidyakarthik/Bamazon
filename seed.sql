459b95a39d747996320451bf24fb4067c1d305c7
nvidya.karthik <nvidya.karthik@gmail.com>
2018-04-22 16:05:51 -0700

    added product_sales colum and new departments table

--- seed.sql
+++ seed.sql
@@ -1,14 +1,14 @@
-INSERT INTO bamazonDB.products VALUES (4123,"Playtex Diaper Genie Refills for Diaper Genie Diaper Pails- 270 Count","Baby",16.03,300);
-INSERT INTO bamazonDB.products(product_name,department_name,price,stock_quantity) VALUES 
-("WaterWipes Sensitive Baby Wipes, 9 Packs of 60 Count (540 Count)","Baby",28.73,300),
-("Bambo Nature Baby Diapers Classic, Size 4 (15-40 lbs), 180 Count (6 Packs of 30)","Baby",71.25,500),
-("Polo Ralph Lauren Men's Faxon Low Sneaker","Shoes",39.95,800),
-("Adidas Men's Seeley Skate Shoe","Shoes",60.22,450),
-("Under Armour Boys' Boys' Leadoff Low RM Jr.","Shoes",27.99,200),
-("Kstare Women Casual Long Sleeve Cotton Solid Loose Pockets T-Shirt Blouses Tops","Clothing",14.59,100),
-("Fashion Womens Plus Size V-Neck T-Shirt Trim Asymmetrical Pullover Patchwork Top","Clothing",8.99,50),
-("Veranee Women's Sleeveless Swing Tunic Summer Floral Flare Tank Top","Clothing",15.25,250),
-("Silk18 Natural Hair Conditioner Argan Oil Sulfate Free Treatment for Dry and Damaged Hair","Personal Care",10.40,400),
-("Moisturizing Shampoo for Dry & Damaged Hair with Lavender & Jojoba ","Personal Care",10.40,120),
-("100% Pure Avocado Oil - Deep Tissue Moisturizer for Hair, Face & Skin","Personal Care",9.45,320),
-("Witch Hazel Toner Alcohol Free Astringent with Aloe Vera and Lavender for Skin ","Personal Care",9.95,900);
+INSERT INTO bamazonDB.products VALUES (4123,"Playtex Diaper Genie Refills for Diaper Genie Diaper Pails- 270 Count","Baby",16.03,300,0);
+INSERT INTO bamazonDB.products(product_name,department_name,price,stock_quantity,product_sales) VALUES 
+("WaterWipes Sensitive Baby Wipes, 9 Packs of 60 Count (540 Count)","Baby",28.73,300,0),
+("Bambo Nature Baby Diapers Classic, Size 4 (15-40 lbs), 180 Count (6 Packs of 30)","Baby",71.25,500,0),
+("Polo Ralph Lauren Men's Faxon Low Sneaker","Shoes",39.95,800,0),
+("Adidas Men's Seeley Skate Shoe","Shoes",60.22,450,0),
+("Under Armour Boys' Boys' Leadoff Low RM Jr.","Shoes",27.99,200,0),
+("Kstare Women Casual Long Sleeve Cotton Solid Loose Pockets T-Shirt Blouses Tops","Clothing",14.59,100,0),
+("Fashion Womens Plus Size V-Neck T-Shirt Trim Asymmetrical Pullover Patchwork Top","Clothing",8.99,50,0),
+("Veranee Women's Sleeveless Swing Tunic Summer Floral Flare Tank Top","Clothing",15.25,250,0),
+("Silk18 Natural Hair Conditioner Argan Oil Sulfate Free Treatment for Dry and Damaged Hair","Personal Care",10.40,400,0),
+("Moisturizing Shampoo for Dry & Damaged Hair with Lavender & Jojoba ","Personal Care",10.40,120,0),
+("100% Pure Avocado Oil - Deep Tissue Moisturizer for Hair, Face & Skin","Personal Care",9.45,320,0),
+("Witch Hazel Toner Alcohol Free Astringent with Aloe Vera and Lavender for Skin ","Personal Care",9.95,900,0);
