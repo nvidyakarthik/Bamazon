6cf195b48b1b14f7c291d0e6d6598946bb1c7d33
nvidya.karthik <nvidya.karthik@gmail.com>
2018-04-23 13:18:18 -0700

    removed dabase connection and updated new product sales column codes

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,23 +1,8 @@
-var mysql = require("mysql");
+var connectionDB = require("./DBconnection");
 var inquirer = require("inquirer");
 var AsciiTable = require('ascii-table');
 var productArray = [];
-//connection to the database
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    // Your username
-    user: "root",
-    // Your password
-    password: "Nvidya1981!",
-    database: "bamazonDB"
-});
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    managerInput();
 
-});
 function managerInput() {
     inquirer.prompt([
         {
@@ -28,16 +13,18 @@ function managerInput() {
         }
 
     ]).then(function (response) {
-        console.log(response.options);
+        //console.log(response.options);
         switch (response.options) {
             case "View Products for Sale":
                 viewProducts();
+                connectionDB.end();
                 break;
             case "View Low Inventory":
                 viewLowInventory();
                 break;
             case "Add to Inventory":
-                addInventory();
+                viewProducts();
+                setTimeout(function () { addInventory(); }, 500);
                 break;
             case "Add New Product":
                 addProduct();
@@ -47,77 +34,78 @@ function managerInput() {
 }
 
 function viewProducts() {
-    connection.query("SELECT * FROM products", function (err, res) {
+    connectionDB.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
         //console.log(res);
         var tableFormat = new AsciiTable('PRODUCT LIST');
         tableFormat.setHeading('ITEM ID', 'PRODUCT NAME', 'PRICE', 'QUANTITES');
-        for (var index in res){
-            
+        for (var index in res) {
+
             tableFormat.addRow(res[index].item_id, res[index].product_name, res[index].price, res[index].stock_quantity);
         }
         console.log(tableFormat.toString());
-        connection.end();
+
 
     });
 }
 
 function viewLowInventory() {
-    connection.query("SELECT * FROM products WHERE stock_quantity <=100", function (err, res) {
+    connectionDB.query("SELECT * FROM products WHERE stock_quantity <=100", function (err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
         var tableFormat = new AsciiTable('INVENTORY LESS THEN 100');
         tableFormat.setHeading('ITEM ID', 'PRODUCT NAME', 'PRICE', 'QUANTITES');
-        for (var index in res){
-            
+        for (var index in res) {
+
             tableFormat.addRow(res[index].item_id, res[index].product_name, res[index].price, res[index].stock_quantity);
         }
         console.log(tableFormat.toString());
-        connection.end();
+        connectionDB.end();
 
 
     });
 }
 
 function getProductNameList(callbackfunc) {
-    
-    connection.query("SELECT product_name FROM products", function (err, res) {
+
+    connectionDB.query("SELECT product_name FROM products", function (err, res) {
         if (err) throw err;
         //console.log(res);
         for (var index in res)
             productArray.push(res[index].product_name);
-            //console.log(productArray);
-            callbackfunc(productArray);       
+        //console.log(productArray);
+        callbackfunc(productArray);
 
     });
 
 }
 
-function getInventory(itemId,callbackfunc){
-    connection.query("SELECT stock_quantity FROM products WHERE item_id=?",[itemId], function (err, res) {
+function getInventory(itemId, callbackfunc) {
+    connectionDB.query("SELECT stock_quantity FROM products WHERE item_id=?", [itemId], function (err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
-       if(res.length!=0){ 
+        if (res.length != 0) {
             callbackfunc(res[0].stock_quantity);
-       }
-       else{
-           console.log("Please enter the correct Item Id");
-           connection.end();
-       }
+        }
+        else {
+            console.log("Item Id Not found");
+            connectionDB.end();
+        }
     });
 
 }
 
-function addInventory(){
-    
+function addInventory() {
+
+
     inquirer.prompt([
 
         {
             type: "input",
             name: "itemId",
             message: "Enter the Item Id ",
-            
+
         },
         {
             type: "input",
@@ -125,47 +113,43 @@ function addInventory(){
             message: "Enter quantity you want to add "
         },
     ]).then(function (response) {
-        getInventory(response.itemId,function(result){
-            console.log("database inventory"+result);
-            var newInventory=result+Number(response.quantity);
-            console.log("new inventory "+newInventory);
-            var query = connection.query(
+        getInventory(response.itemId, function (result) {
+            //console.log("database inventory"+result);
+            var newInventory = result + Number(response.quantity);
+            //console.log("new inventory "+newInventory);
+            var query = connectionDB.query(
                 "UPDATE products SET ? WHERE ?",
                 [
-                  {
-                    stock_quantity: newInventory
-                  },
-                  {
-                    item_id: response.itemId
-                  }
-                ],  
-                function(err, res) {
-                  //console.log(err);
-                  console.log(res.affectedRows + " products updated!\n");
-                  connection.end();
-                  
+                    {
+                        stock_quantity: newInventory
+                    },
+                    {
+                        item_id: response.itemId
+                    }
+                ],
+                function (err, res) {
+                    //console.log(err);
+                    //console.log(res.affectedRows + " products updated!\n");
+                    if (res.affectedRows == 1) {
+                        console.log("Inventory updated!!");
+                    }
+                    connectionDB.end();
+
                 }
-              );
-    
-
-
+            );
         });
-        
-        
-
-
-    }); 
+    });
 
 }
 
-function addProduct(){
+function addProduct() {
     inquirer.prompt([
 
         {
             type: "input",
             name: "productName",
             message: "Enter the Product Name ",
-            
+
         },
         {
             type: "input",
@@ -175,73 +159,62 @@ function addProduct(){
         {
             type: "input",
             name: "price",
-            message: "Enter the Price "
+            message: "Enter the Price ",
+            validate: function (value) {
+                if (isNaN(value) === true) {
+                    console.log("\nPlease enter a Valid number.");
+                    return false;
+                }
+                else {
+                    return true;
+                }
+
+            }
         },
         {
             type: "input",
             name: "quantity",
-            message: "Enter total quantity "
-        }
+            message: "Enter total quantity ",
+            validate: function (value) {
+                if (isNaN(value) === true) {
+                    console.log("\nPlease enter correct Quantity.");
+                    return false;
+                }
+                else if (Number.isInteger(Number(value)) === false && Number(value)<=0) {
+                    console.log("\nPlease enter correct Quantity.");
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        },
+
     ]).then(function (response) {
-        var query = connection.query(
+        var query = connectionDB.query(
             "INSERT INTO products SET ?",
             {
-              product_name: response.productName,
-              department_name: response.departmentName,
-              price: response.price,
-              stock_quantity:response.quantity
+                product_name: response.productName,
+                department_name: response.departmentName,
+                price: response.price,
+                stock_quantity: response.quantity
             },
-            function(err, res) {
-                
-              console.log(res.affectedRows + " product inserted!\n");
+            function (err, res) {
+                if (res.affectedRows == 1) {
+                    console.log("New product added!!");
+                    connectionDB.end();
+                }
+                else {
+                    console.log("Some error has occurred.Try again");
+                    connectionDB.end();
+                }
+
             }
-          );
-        
-          // logs the actual query being run
-          console.log(query.sql);
+        );
+
     });
 
 }
 
-/* function addInventory() {
-    
-     getProductNameList(function(getProductNames){
-        inquirer.prompt([
+managerInput();
 
-            {
-                type: "list",
-                name: "productName",
-                message: "Please Select from options..",
-                choices: getProductNames
-            },
-            {
-                type: "input",
-                name: "quantity",
-                message: "Enter quantity you want to add"
-            },
-        ]).then(function (response) {
-
-            var query = connection.query(
-                "UPDATE products SET ? WHERE ?",
-                [
-                  {
-                    stock_quantity: response.quantity
-                  },
-                  {
-                    product_name: response.productName
-                  }
-                ],
-                function(err, res) {
-                  console.log(err);
-                  console.log(res.affectedRows + " products updated!\n");
-                  connection.end();
-                  
-                }
-              );
-    
-    
-    
-        }); 
-       
-     });    
-} */
