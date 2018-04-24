d0f9886c2fc2ab716f5c3728b259b00985e13f12
nvidya.karthik <nvidya.karthik@gmail.com>
2018-04-24 12:01:51 -0700

    view all department name added

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -8,11 +8,11 @@ function supervisorInput() {
             type: "list",
             name: "options",
             message: "Please Select from options..",
-            choices: ["View Product Sales by Department", "Create new Department"]
+            choices: ["View Product Sales by Department", "Create new Department","View all Departments"]
         }
 
     ]).then(function (response) {
-        console.log(response.options);
+        //console.log(response.options);
         switch (response.options) {
             case "View Product Sales by Department":
                 viewSalesByDept();
@@ -22,9 +22,31 @@ function supervisorInput() {
                 createDept();
                 
                 break;
+            case "View all Departments":
+                viewAllDept();
+                
+                break;    
             
         }
     });
+}
+
+function viewAllDept(){
+    connectionDB.query("SELECT department_name FROM departments", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        var tableFormat = new AsciiTable();
+        tableFormat.setHeading('DEPARTMENT NAME');
+        for (var index in res){
+            
+            tableFormat.addRow(res[index].department_name);
+        }
+        console.log(tableFormat.toString());
+        connectionDB.end();
+
+
+    });
+
 }
 function viewSalesByDept(){
     connectionDB.query("select departments.department_id,departments.department_name,departments.over_head_costs,SUM(products.product_sales) as product_sales,(SUM(product_sales)-departments.over_head_costs) as total_profit from departments inner join products where departments.department_name=products.department_name group by products.department_name;", function (err, res) {
