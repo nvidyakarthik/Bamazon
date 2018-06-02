e1ce083010c16ae9594d16140812a9a5762b485d
nvidya.karthik <nvidya.karthik@gmail.com>
2018-06-02 15:59:27 -0700

    added separate schema and seeds for supervisor view

--- schema.sql
+++ schema.sql
@@ -4,6 +4,19 @@ CREATE DATABASE bamazonDB;
 
 USE bamazonDB;
 
+-- Use this Schema for the Customer View and Manager View Challenge
+
+CREATE TABLE products(
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR(100) NOT NULL,
+department_name VARCHAR(50) NOT NULL,
+price DECIMAL(10,2) NOT NULL,
+stock_quantity INT NOT NULL,
+PRIMARY KEY(item_id)
+);
+
+-- Use this Schema for the Supervisor View  Challenge
+-- product_sales column is added for the Supervisor View Challenge
 CREATE TABLE products(
 item_id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(100) NOT NULL,
